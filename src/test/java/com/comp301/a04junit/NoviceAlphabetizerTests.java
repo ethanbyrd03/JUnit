2e657fb0c02482e74bc5b5a1bff70b0bea3a7bec
Ethan Byrd <ethbyr@ad.unc.edu>
2023-10-11 13:05:58 -0400

    idk

--- src/test/java/com/comp301/a04junit/NoviceAlphabetizerTests.java
+++ src/test/java/com/comp301/a04junit/NoviceAlphabetizerTests.java
@@ -6,10 +6,50 @@ import com.comp301.a04junit.alphabetizer.Alphabetizer;
 
 import org.junit.Test;
 
-/** Write tests for the Alphabetizer class here */
+import java.util.NoSuchElementException;
+
+/**
+ * Write tests for the Alphabetizer class here
+ */
 public class NoviceAlphabetizerTests {
-  @Test
-  public void unitTest1() {
-    assertTrue(true); // TODO: Write your first unit test!
-  }
+    @Test
+    public void testConstructor() {
+        String[] a = new String[1];
+        a[0] = "a";
+        Alphabetizer test = new Alphabetizer(a);
+    }
+
+    @Test
+    public void testNext() {
+        String[] testStr = new String[3];
+        testStr[0] = "b1";
+        testStr[1] = "a1";
+        testStr[2] = "c1";
+        Alphabetizer test = new Alphabetizer(testStr);
+        assertTrue(test.next() == "a1");
+    }
+
+    @Test
+    public void testNextNoNext() {
+        String[] testStr = new String[3];
+        testStr[0] = "b1";
+        testStr[1] = "a1";
+        testStr[2] = "c1";
+        Alphabetizer test = new Alphabetizer(testStr);
+        try {
+            test.next();
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testHasNext() {
+        String[] testStr = new String[3];
+        testStr[0] = "b1";
+        testStr[1] = "a1";
+        testStr[2] = "c1";
+        Alphabetizer test = new Alphabetizer(testStr);
+        assertTrue(test.hasNext());
+    }
 }
