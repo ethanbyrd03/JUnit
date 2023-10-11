9dd96573a66c419f133bc929d49fb4b092447c60
Ethan Byrd <ethbyr@ad.unc.edu>
2023-10-11 18:47:49 -0400

    Item tests

--- src/test/java/com/comp301/a04junit/AdeptItemTests.java
+++ src/test/java/com/comp301/a04junit/AdeptItemTests.java
@@ -7,29 +7,31 @@ import com.comp301.a04junit.adventure.ItemImpl;
 
 import org.junit.Test;
 
-/** Write unit tests for the ItemImpl class here */
-public class  AdeptItemTests {
-  @Test
-  public void testGetName() {
-    String testName = "fungus";
-    Item test = new ItemImpl(testName);
-    assertTrue(test.getName() == testName);
-  }
+/**
+ * Write unit tests for the ItemImpl class here
+ */
+public class AdeptItemTests {
+    @Test
+    public void testGetName() {
+        String testName = "fungus";
+        Item test = new ItemImpl(testName);
+        assertTrue(test.getName() == testName);
+    }
 
-  @Test
-  public void testEqualsNotEqual() {
-    String test1Name = "fungus";
-    String test2Name = "patch";
-    Item test1 = new ItemImpl(test1Name);
-    Item test2 = new ItemImpl(test2Name);
-    assertTrue(!test1.equals(test2));
-  }
+    @Test
+    public void testEqualsNotEqual() {
+        String test1Name = "fungus";
+        String test2Name = "patch";
+        Item test1 = new ItemImpl(test1Name);
+        Item test2 = new ItemImpl(test2Name);
+        assertTrue(!test1.equals(test2));
+    }
 
-  @Test
-  public void testEqualsIsEqual() {
-    String test1Name = "fungus";
-    Item test1 = new ItemImpl(test1Name);
-    Item test2 = new ItemImpl(test1Name);
-    assertTrue(test1.equals(test2));
-  }
+    @Test
+    public void testEqualsIsEqual() {
+        String test1Name = "fungus";
+        Item test1 = new ItemImpl(test1Name);
+        Item test2 = new ItemImpl(test1Name);
+        assertTrue(test1.equals(test2));
+    }
 }
