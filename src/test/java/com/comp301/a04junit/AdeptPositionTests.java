fe9776cf431e390d321c9652ed197d4d315f65c1
Ethan Byrd <ethbyr@ad.unc.edu>
2023-10-12 12:38:06 -0400

    Position tests

--- src/test/java/com/comp301/a04junit/AdeptPositionTests.java
+++ src/test/java/com/comp301/a04junit/AdeptPositionTests.java
@@ -2,15 +2,56 @@ package com.comp301.a04junit;
 
 import static org.junit.Assert.assertTrue;
 
+import com.comp301.a04junit.adventure.Direction;
 import com.comp301.a04junit.adventure.Position;
 import com.comp301.a04junit.adventure.PositionImpl;
 
 import org.junit.Test;
 
-/** Write unit tests for the PositionImpl class here */
+/**
+ * Write unit tests for the PositionImpl class here
+ */
 public class AdeptPositionTests {
-  @Test
-  public void unitTest1() {
-    assertTrue(true); // TODO: Write your first unit test!
-  }
+    @Test
+    public void testPositionXY() {
+        int testX = 1;
+        int testY = 2;
+        Position testPos = new PositionImpl(testX, testY);
+        testX = 0;
+        testY = 0;
+        assertTrue(testPos.getX() == 1);
+        assertTrue(testPos.getY() == 2);
+    }
+
+    @Test
+    public void testPositionNorth() {
+        int testX = 1;
+        int testY = 2;
+        Position testPos = new PositionImpl(testX, testY);
+        assertTrue(testPos.getNeighbor(Direction.NORTH).getY() == testPos.getY() + 1);
+    }
+
+    @Test
+    public void testPositionSouth() {
+        int testX = 1;
+        int testY = 2;
+        Position testPos = new PositionImpl(testX, testY);
+        assertTrue(testPos.getNeighbor(Direction.SOUTH).getY() == testPos.getY() - 1);
+    }
+
+    @Test
+    public void testPositionNEast() {
+        int testX = 1;
+        int testY = 2;
+        Position testPos = new PositionImpl(testX, testY);
+        assertTrue(testPos.getNeighbor(Direction.EAST).getX() == testPos.getX() + 1);
+    }
+
+    @Test
+    public void testPositionWest() {
+        int testX = 1;
+        int testY = 2;
+        Position testPos = new PositionImpl(testX, testY);
+        assertTrue(testPos.getNeighbor(Direction.WEST).getX() == testPos.getX() - 1);
+    }
 }
